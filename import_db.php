3b36111db0a89178e55bd4c5ae7705ca993673b9
Daniel Luca <lucadanielcostin@gmail.com>
2015-09-13 07:50:08 +0300

    Implement import for copayers also

--- import_db.php
+++ import_db.php
@@ -24,3 +24,24 @@ while( !feof($applicants_file) )
 
 }
 
+$copayers_file = fopen('/tmp/copayers.txt', 'r');
+
+while( !feof($copayers_file) )
+{
+	$copayer = fgetcsv($copayers_file, 0, '|');
+
+	$last_name = "";
+	$first_name = "";
+
+	$name_parts = explode(" ", $copayer[1]);
+
+	$last_name = $name_parts[0];
+	$first_name = implode(" ", array_slice($name_parts, 1));
+
+	echo "first_name = $first_name \n";
+	echo "last_name = $last_name \n";
+	echo "\n";
+
+	$db->query("UPDATE `copayers` SET `first_name`='{$first_name}', `last_name`='{$last_name}' WHERE `id`='{$copayer[0]}'");
+
+}
